20b9922059ee10a18d71caff878b1ae81a2cc527
Hải Phạm <9427294+haipham22@users.noreply.github.com>
2020-09-11 14:49:17 +0700

    add group method

--- app/Listeners/CreateNotifyRecord.php
+++ app/Listeners/CreateNotifyRecord.php
@@ -74,13 +74,13 @@ class CreateNotifyRecord
 	                (
                         SELECT :notification_id as notification_id, (SELECT id FROM notification_devices nd WHERE user_id = u.id) as device_id, u.id as user_id, 'PENDING' as `status`, 0 as is_read, NOW() as created_at, NOW() as updated_at
                         FROM users AS u
-                        WHERE u.is_active = 1 AND id in :user_ids
+                        WHERE u.is_active = 1 AND u.phone in (:user_phone)
                         ORDER BY created_at DESC
 	                )";
 
         DB::insert($query, [
             'notification_id' => $notification->id,
-            'user_ids' => $notification->receiver_id
+            'user_phone' => (string) $notification->receiver_id
         ]);
 
         $notificationRecords = NotificationRecord::query()
